0d8adf7e92c8d596111165d1eeb6cac3aa35cc45
bullerun1 <ryskalnikita2004@mail.ru>
2024-01-20 09:00:24 +0300

    main page done +-

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import {MainComponent} from "./core/pages/main/main.component";
 
 
 export const routes: Routes = [
-
+  {path: '', component: MainComponent, pathMatch:"full"}
 ];
